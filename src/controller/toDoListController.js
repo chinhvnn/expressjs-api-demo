ff1cdd7ff323e1417c30c48b6862a73d3688c77e
chinhvnn <chinhqn09@gmail.com>
2023-02-28 16:06:13 +0700

    fix CORS

--- src/controller/toDoListController.js
+++ src/controller/toDoListController.js
@@ -1,10 +1,12 @@
 const fakeData = require('../fakeData');
 
 const getTasks = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.json(fakeData.toDoList);
 };
 
 const getTaskById = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.json(fakeData.toDoList.filter((task) => task.id == req.params.id));
 };
 
