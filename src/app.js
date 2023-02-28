e0ab3e1c12fdb4e6dd2263fef98b45dd71ee065c
chinhvnn <chinhqn09@gmail.com>
2023-02-28 10:44:38 +0700

    fix netlify

--- src/app.js
+++ src/app.js
@@ -5,13 +5,8 @@ const apiV1Router = require('./routes/apiV1Router');
 
 
 const app = express();
-const port = 3000;
 
 app.use(bodyParser.json())
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
 
 apiV1Router(app);
 
