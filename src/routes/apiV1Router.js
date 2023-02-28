5a25b9f29e688aed3f72d004d704144a80d991a9
chinhvnn <chinhqn09@gmail.com>
2023-02-28 13:50:29 +0700

    fix netlify

--- src/routes/apiV1Router.js
+++ src/routes/apiV1Router.js
@@ -7,7 +7,7 @@ const apiV1Router = (app) => {
   router.get('/', getTasks);
   router.get('/hello', (req, res) => res.send('Hello'));
 
-  return app.use('/.netlify/functions/api/v1', router);
+  return app.use('/.netlify/functions/app/api/v1', router);
 };
 
 module.exports = apiV1Router;
